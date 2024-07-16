ffa8a9d64d8128ae732ce2118655ce9bfe1cd976
Jakub Olcha <olo75jg12@gmail.com>
2024-07-16 19:27:46 +0200

    complete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rename-files-in-directory-script
+
+Simle script that I used for renaming epub files
+
+# Usage
+
+go run main.go path_to_directory
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func renameFile(oldPath, newPath string) {
+	err := os.Rename(oldPath, newPath)
+	if err != nil {
+		fmt.Printf("Error renaming file %s to %s: %v\n", oldPath, newPath, err)
+	} else {
+		fmt.Printf("File renamed from %s to %s\n", oldPath, newPath)
+	}
+}
+
+func renameFilesInDirectory(directory string) {
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		fmt.Printf("Error reading directory: %v\n", err)
+		return
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			oldPath := filepath.Join(directory, file.Name())
+			newFileName := strings.ReplaceAll(file.Name(), "_", " ")
+			newPath := filepath.Join(directory, newFileName)
+			renameFile(oldPath, newPath)
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide the directory as an argument.")
+		os.Exit(1)
+	}
+
+	directory := os.Args[1]
+	// fmt.Printf("%s", directory)
+
+	renameFilesInDirectory(directory)
+}
